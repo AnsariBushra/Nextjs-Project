26a5964b46587e1552f5b26701f6e6706c44aa3b
AnsariBushra <bushraansari.0517@gmail.com>
2025-03-27 23:12:41 +0530

    Fix deployment issue

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true, // Enable App Router if using it
-  },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
